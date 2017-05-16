ee6e743ff4d0b7f53c70ed3fdefb26c9eed6813d
Mohammd Sharifi <mmdsharifi@yahoo.com>
2017-05-16 12:42:12 +0430

    Init Commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello Travis!\n') // this will FAIL travis ci lint
+
+    })
+    .listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
